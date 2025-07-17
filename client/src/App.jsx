9eda34b1b98d4c62ec4fe00fad819edc553afd63
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-07-17 14:41:52 +0530

    fix: the error causing reciped to not be stored

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,12 +9,18 @@ import RecipeList from "./components/RecipeList";
 function App() {
   const [recipes, setRecipes] = useState([]);
 
+  const API_URL =
+    import.meta.env.DEV
+      ? "http://localhost:3001"
+      : import.meta.env.VITE_API_URL;
+
   useEffect(() => {
-    fetch("http://localhost:3001/api/recipes")
+    fetch(`${API_URL}/api/recipes`)
       .then((res) => res.json())
       .then((data) => setRecipes(data))
       .catch((err) => console.error("Error fetching recipes:", err));
   }, []);
+
 
   const addRecipe = (newRecipe) => {
     setRecipes((prev) => [newRecipe, ...prev]);
